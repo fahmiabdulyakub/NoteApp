d09cb29b5584bc5f7861ad620b44917fe9782fd7
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2023-10-03 23:28:56 +0700

    add themes

---
+++ src/themes/Colors.ts
@@ -0,0 +1,14 @@
+const Colors = {
+  blue: '#0652A3',
+  border_danger: '#FF7040',
+  black: '#1C1D21',
+  white: '#FFFFFF',
+  light_grey: '#E0E0E0',
+  background: '#F9F9F9',
+  green: '#479647',
+  dark_grey: '#C2C2C8',
+  gray: '#969696',
+  red: '#F30A0A',
+};
+
+export default Colors;
