a2b4fa8356531d926d6c3302c42064c77e7861be
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2023-10-05 02:24:08 +0700

    fix null title

--- src/components/molecules/NoteItem/index.tsx
+++ src/components/molecules/NoteItem/index.tsx
@@ -4,15 +4,21 @@ import styles from './styles';
 
 import {NoteType} from '@types';
 import useNoteItem from './hooks/useNoteItem';
+import dayjs from 'dayjs';
 
 const NoteItem = (props: NoteType) => {
   const {onPress} = useNoteItem(props);
+
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
       <View style={styles.contenContainer}>
         <Text style={styles.description}>{props.description}</Text>
       </View>
-      <Text style={styles.title}>{props.title}</Text>
+      <Text style={styles.title}>
+        {!props.title
+          ? 'Note ' + dayjs(props.date).format('DD/MM/YYYY')
+          : props.title}
+      </Text>
     </TouchableOpacity>
   );
 };
